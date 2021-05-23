c205b95b805881d381484ca972a06d34e24029e4
thalalhassan <thalal424@gmail.com>
2021-05-23 20:17:35 +0530

    build updates

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/vue-covid-traker/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-covid-traker/" : "/",
 };
